945cbcb05183970043fc51a761e06965b9ee396d
Develing1991 <completed0728@gmail.com>
2023-01-13 19:49:28 +0900

    정적, 동적 배포분기처리

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   generateBuildId: () => "suhanLee_deploy_build_Id",
+
+  // out 폴더 (정적 페이지) 빌드할 목록 명시
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "/404" },
+  }),
 };
 
 module.exports = nextConfig;
