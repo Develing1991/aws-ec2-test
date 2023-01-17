d1ea9b27160a7272501af9bc5dfb384e1d54dd27
Develing1991 <completed0728@gmail.com>
2023-01-18 01:21:38 +0900

    dsf

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,15 @@
 import "../styles/globals.css";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+// import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
-const globalApolloState = new InMemoryCache();
+//const globalApolloState = new InMemoryCache();
 export default function App({ Component, pageProps }) {
-  const client = new ApolloClient({
-    uri: `https://backendonline.codebootcamp.co.kr/graphql`,
-    cache: globalApolloState,
-  });
+  // const client = new ApolloClient({
+  //   uri: `https://backendonline.codebootcamp.co.kr/graphql`,
+  //   cache: globalApolloState,
+  // });
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    // <ApolloProvider client={client}>
+    // </ApolloProvider>
+    <Component {...pageProps} />
   );
 }
--- pages/boards/[boardId]/index.jsx
+++ pages/boards/[boardId]/index.jsx
@@ -1,32 +1,37 @@
-import { useQuery } from "@apollo/client";
-import { gql } from "graphql-request";
+// import { useQuery } from "@apollo/client";
+// import { gql } from "graphql-request";
+import axios from "axios";
 import { useRouter } from "next/router";
 
-const FETCH_BOARD = gql`
-  query fetchBoard($boardId: ID!) {
-    fetchBoard(boardId: $boardId) {
-      _id
-      writer
-      title
-      contents
-    }
-  }
-`;
+// const FETCH_BOARD = gql`
+//   query fetchBoard($boardId: ID!) {
+//     fetchBoard(boardId: $boardId) {
+//       _id
+//       writer
+//       title
+//       contents
+//     }
+//   }
+// `;
 
 export default function BoardPage() {
   const router = useRouter();
-  const { data } = useQuery(FETCH_BOARD, {
-    variables: {
-      boardId: String("63c63fe61182750028ed0f76"),
-    },
-  });
-  const aaa = () => {
+  // const { data } = useQuery(FETCH_BOARD, {
+  //   variables: {
+  //     boardId: String("63c63fe61182750028ed0f76"),
+  //   },
+  // });
+  const aaa = async () => {
+    const result = await axios.get(
+      "https://www.fishwatch.gov/api/species/red-snapper"
+    );
+    console.log(result);
     alert("fdsd");
   };
   return (
     <div>
-      <div>{data?.fetchBoard.writer}</div>
-      <div>{data?.fetchBoard.contents}</div>
+      {/* <div>{data?.fetchBoard.writer}</div>
+      <div>{data?.fetchBoard.contents}</div> */}
       <button onClick={aaa}>ㅅㅅㅅ</button>
       안녕. 동적페이지 테스트 <br />
       동적페이지 게시글 아이디 : {router.query.boardId}
